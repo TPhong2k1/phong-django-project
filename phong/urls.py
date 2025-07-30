9976e25c6b89366f798e51b34cf16acdb4a2f09d
Phong <phongthanh.79590@gmail.com>
2025-07-30 08:36:41 +0700

    khac phuc loi admin

--- phong/urls.py
+++ phong/urls.py
@@ -22,10 +22,10 @@ from django.contrib.auth import get_user_model
 def create_super_user():
     User = get_user_model()
     if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+        User.objects.create_superuser('admin', 'banchihuy@gmail.com', 'banchihuy@')
         print("✅ Superuser 'admin' đã được tạo!")
 
-create_super_user()
+# create_super_user()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
